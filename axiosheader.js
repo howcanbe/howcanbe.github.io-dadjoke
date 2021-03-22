a78fcc811de11890dbab37527896733092384043
howcanbe <59401518+howcanbe@users.noreply.github.com>
2021-03-22 12:12:43 -0700

    Add files via upload

---
+++ axiosheader.js
@@ -0,0 +1,19 @@
+let jokes = document.querySelector('#jokes');
+const button = document.querySelector('button')
+
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLI = document.createElement('LI');
+    newLI.append(jokeText);
+    jokes.append(newLI)
+}
+
+const getDadJoke = async () => {
+    const config = { headers: { Accept: 'application/json'} }
+    const res = await axios.get('https://icanhazdadjoke.com/', config)
+    return res.data.joke;
+}
+
+button.addEventListener('click', addNewJoke)
+
